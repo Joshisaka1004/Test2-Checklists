125887d6ffa1e2f8f9b8cea63645730744cbea5b
Joachim Vetter <JoachimVetter@joachims-mbp>
2017-06-11 15:53:29 +0200

    Initial Commit

---
+++ Checklists-170611Tests/Checklists_170611Tests.swift
@@ -0,0 +1,36 @@
+//
+//  Checklists_170611Tests.swift
+//  Checklists-170611Tests
+//
+//  Created by Joachim Vetter on 11.06.17.
+//  Copyright © 2017 Joachim Vetter. All rights reserved.
+//
+
+import XCTest
+@testable import Checklists_170611
+
+class Checklists_170611Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
