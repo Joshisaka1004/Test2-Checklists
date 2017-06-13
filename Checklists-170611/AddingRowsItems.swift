fa79ff9ce2fe8ca69c44d2064fc86d593703f6c0
Joachim Vetter <JoachimVetter@joachims-mbp>
2017-06-13 11:48:06 +0200

    Some first IB additions

---
+++ Checklists-170611/AddingRowsItems.swift
@@ -0,0 +1,18 @@
+//
+//  AddingRowsItems.swift
+//  Checklists-170611
+//
+//  Created by Joachim Vetter on 13.06.17.
+//  Copyright © 2017 Joachim Vetter. All rights reserved.
+//
+
+import UIKit
+
+class AddingRowsItems: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+}
